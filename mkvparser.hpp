4e0255601e7e361a0ef5d025cfeb4a1c3bcf1042
matthewjheaney <matthewjheaney@google.com>
2012-02-27 22:21:58 -0500

    Parse headers outside of constructor
    In several cases, the parser would parse a header
    (say, a track header, or the segment info header)
    in the constructor for the type.  The parser had
    assumed (incorrectly) that the file was well-formed,
    but this turned out to be an incorrect assumption.
    
    The parse errors triggered some assertion failures,
    but this is not acceptable in a production system.
    
    The parser solved that problem by separating the
    construction of the header object from the parsing
    of the file.  There is now a separate parse method
    to be called after construction of the object,
    which returns a status whether the parse was
    successful.
    
    This change fixes the bugs from the following
    tracker issues:
    
    http://code.google.com/p/webm/issues/detail?id=399
    http://code.google.com/p/webm/issues/detail?id=400
    
    Change-Id: Idb09154ae7008429d8613ce3b3e8294f5a12de86

--- mkvparser.hpp
+++ mkvparser.hpp
@@ -32,15 +32,26 @@ long long GetUIntLength(IMkvReader*, long long, long&);
 long long ReadUInt(IMkvReader*, long long, long&);
 long long SyncReadUInt(IMkvReader*, long long pos, long long stop, long&);
 long long UnserializeUInt(IMkvReader*, long long pos, long long size);
+
 float Unserialize4Float(IMkvReader*, long long);
 double Unserialize8Double(IMkvReader*, long long);
+long UnserializeFloat(IMkvReader*, long long pos, long long size, double&);
 
-#if 0
-short Unserialize2SInt(IMkvReader*, long long);
-signed char Unserialize1SInt(IMkvReader*, long long);
-#else
 long UnserializeInt(IMkvReader*, long long pos, long len, long long& result);
-#endif
+
+long UnserializeString(
+        IMkvReader*,
+        long long pos,
+        long long size,
+        char*& str,
+        bool delete_str_on_entry = true);
+
+long ParseElementHeader(
+    IMkvReader* pReader,
+    long long& pos,  //consume id and size fields
+    long long stop,  //if you know size of element's parent
+    long long& id,
+    long long& size);
 
 bool Match(IMkvReader*, long long&, unsigned long, long long&);
 bool Match(IMkvReader*, long long&, unsigned long, char*&);
@@ -289,6 +300,8 @@ class Track
     Track& operator=(const Track&);
 
 public:
+    enum Type { kVideo = 1, kAudio = 2 };
+
     Segment* const m_pSegment;
     const long long m_element_start;
     const long long m_element_size;
@@ -344,6 +357,7 @@ protected:
         const Info&,
         long long element_start,
         long long element_size);
+
     const Info m_info;
 
     class EOSBlock : public BlockEntry
@@ -368,12 +382,21 @@ class VideoTrack : public Track
     VideoTrack(const VideoTrack&);
     VideoTrack& operator=(const VideoTrack&);
 
-public:
     VideoTrack(
         Segment*,
         const Info&,
         long long element_start,
         long long element_size);
+
+public:
+    static long Parse(
+        Segment*,
+        const Info&,
+        long long element_start,
+        long long element_size,
+        VideoTrack*&,
+        bool delete_track_on_entry = false);
+
     long long GetWidth() const;
     long long GetHeight() const;
     double GetFrameRate() const;
@@ -394,12 +417,20 @@ class AudioTrack : public Track
     AudioTrack(const AudioTrack&);
     AudioTrack& operator=(const AudioTrack&);
 
-public:
     AudioTrack(
         Segment*,
         const Info&,
         long long element_start,
         long long element_size);
+public:
+    static long Parse(
+        Segment*,
+        const Info&,
+        long long element_start,
+        long long element_size,
+        AudioTrack*&,
+        bool delete_track_on_entry = false);
+
     double GetSamplingRate() const;
     long long GetChannels() const;
     long long GetBitDepth() const;
@@ -431,7 +462,12 @@ public:
         long long size,
         long long element_start,
         long long element_size);
-    virtual ~Tracks();
+
+    ~Tracks();
+
+    long Parse();
+
+    unsigned long GetTracksCount() const;
 
     const Track* GetTrackByNumber(unsigned long tn) const;
     const Track* GetTrackByIndex(unsigned long idx) const;
@@ -440,15 +476,13 @@ private:
     Track** m_trackEntries;
     Track** m_trackEntriesEnd;
 
-    void ParseTrackEntry(
-        long long,
-        long long,
-        Track*&,
+    long ParseTrackEntry(
+        long long payload_start,
+        long long payload_size,
         long long element_start,
-        long long element_size);
+        long long element_size,
+        Track*&) const;
 
-public:
-    unsigned long GetTracksCount() const;
 };
 
 
@@ -472,6 +506,8 @@ public:
         long long element_size);
 
     ~SegmentInfo();
+
+    long Parse();
 
     long long GetTimeCodeScale() const;
     long long GetDuration() const;  //scaled
@@ -508,6 +544,8 @@ public:
         long long element_size);
 
     ~SeekHead();
+
+    long Parse();
 
     struct Entry
     {

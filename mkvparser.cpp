f1bb86a3759eaccc7b4ede041c681c8e5b7d13d6
Leonel Togniolli <togniolli@gmail.com>
2015-04-18 23:59:19 +0100

    Fixes issues on Block Group parsing
    Changed assertions to "invalid file format" errors files that
    had Block Groups without Block IDs or had invalid durations
    
    Change-Id: I1383a63bfb76ee46aaa0aff089780383ce65fc31

--- mkvparser.cpp
+++ mkvparser.cpp
@@ -6447,10 +6447,13 @@ long Cluster::CreateBlockGroup(long long start_offset, long long size,
         bsize = size;
       }
     } else if (id == 0x1B) {  // Duration ID
-      assert(size <= 8);
+      if (size > 8)
+        return E_FILE_FORMAT_INVALID;
 
       duration = UnserializeUInt(pReader, pos, size);
-      assert(duration >= 0);  // TODO
+
+      if (duration < 0)
+        return E_FILE_FORMAT_INVALID;
     } else if (id == 0x7B) {  // ReferenceBlock
       assert(size <= 8);
       const long size_ = static_cast<long>(size);
@@ -6471,9 +6474,10 @@ long Cluster::CreateBlockGroup(long long start_offset, long long size,
     pos += size;  // consume payload
     assert(pos <= stop);
   }
+  if (bpos < 0)
+    return E_FILE_FORMAT_INVALID;
 
   assert(pos == stop);
-  assert(bpos >= 0);
   assert(bsize >= 0);
 
   const long idx = m_entries_count;

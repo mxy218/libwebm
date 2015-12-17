48b5e5eed2429aea39f29bdb22728f0abd450880
Tom Finegan <tomfinegan@google.com>
2015-12-16 16:19:59 -0800

    WIP/DO NOT REVIEW: webm2ts: Converts WebM VPx video to a MPEG TS.
    Built atop webm2pes, takes the output from webm2pes and packetizes it
    into MPEG TS.
    
    Change-Id: Ia122479ee91a112ad7fe223a571ca6d7ba66d406

--- webm2pes.cc
+++ webm2pes.cc
@@ -305,6 +305,61 @@ bool Webm2Pes::ConvertToFile() {
   return true;
 }
 
+bool Webm2Pes::ConvertToTypedPacketStream() {
+  if (input_file_name_.empty() || packet_sink_ == nullptr) {
+    std::fprintf(stderr, "Webm2Pes: input file name empty or null sink.\n");
+    return false;
+  }
+
+  if (InitWebmParser() != true) {
+    std::fprintf(stderr, "Webm2Pes: Cannot initialize WebM parser.\n");
+    return false;
+  }
+
+  // Walk clusters in segment.
+  const mkvparser::Cluster* cluster = webm_parser_->GetFirst();
+  while (cluster != nullptr && cluster->EOS() == false) {
+    const mkvparser::BlockEntry* block_entry = nullptr;
+    std::int64_t block_status = cluster->GetFirst(block_entry);
+    if (block_status < 0) {
+      std::fprintf(stderr, "Webm2Pes: Cannot parse first block in %s.\n",
+                   input_file_name_.c_str());
+      return false;
+    }
+
+    // Walk blocks in cluster.
+    while (block_entry != nullptr && block_entry->EOS() == false) {
+      const mkvparser::Block* block = block_entry->GetBlock();
+      if (block->GetTrackNumber() == video_track_num_) {
+        const int frame_count = block->GetFrameCount();
+
+        // Walk frames in block.
+        for (int frame_num = 0; frame_num < frame_count; ++frame_num) {
+          const mkvparser::Block::Frame& frame = block->GetFrame(frame_num);
+
+          // Write frame out as PES packet(s).
+          const bool pes_status =
+              WritePesPacket(frame, block->GetTime(cluster));
+          if (pes_status != true) {
+            std::fprintf(stderr, "Webm2Pes: WritePesPacket failed.\n");
+            return false;
+          }
+        }
+      }
+      block_status = cluster->GetNext(block_entry, block_entry);
+      if (block_status < 0) {
+        std::fprintf(stderr, "Webm2Pes: Cannot parse block in %s.\n",
+                     input_file_name_.c_str());
+        return false;
+      }
+    }
+
+    cluster = webm_parser_->GetNext(cluster);
+  }
+
+  return true;
+}
+
 bool Webm2Pes::InitWebmParser() {
   if (webm_reader_.Open(input_file_name_.c_str()) != 0) {
     std::fprintf(stderr, "Webm2Pes: Cannot open %s as input.\n",

83d39732751192891377bf7bbbb33b746989a21a
Tom Finegan <tomfinegan@google.com>
2016-04-01 11:58:28 -0400

    Support cross compile for windows via mingw64.
    BUG=https://bugs.chromium.org/p/webm/issues/detail?id=1170
    
    Change-Id: I8430600ceb610fbf1ecff34f0a2d2c1a57627692

---
+++ build/mingw64_toolchain.cmake
@@ -0,0 +1,21 @@
+##  Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+##
+##  Use of this source code is governed by a BSD-style license
+##  that can be found in the LICENSE file in the root of the source
+##  tree. An additional intellectual property rights grant can be found
+##  in the file PATENTS.  All contributing project authors may
+##  be found in the AUTHORS file in the root of the source tree.
+
+# Target OS name.
+SET(CMAKE_SYSTEM_NAME Windows)
+
+# Cross compiler values.
+SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+
+# Tell CMake not to look too hard for things; we want the target toolchain
+# defaults.
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)

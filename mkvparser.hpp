747558f8ed19174b880cef8526353a46dd92e95b
Tom Finegan <tomfinegan@google.com>
2016-04-11 11:37:29 -0700

    Add temporary include shims at old file locations.
    Helps ease the downstream transition to the new libwebm layout.
    
    Change-Id: I0d4182e22a10fecdd5982b8aceba687dea9529d8

---
+++ mkvparser.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2016 The WebM project authors. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS.  All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+
+// This file is a wrapper for the file included immediately after this comment.
+// New projects should not include this file: include the file included below.
+#include "mkvparser/mkvparser.h"

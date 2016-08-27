07c37a1deb408211ef4ebf0cbd16f2ed4b5bf2bc
Tom Finegan <tomfinegan@google.com>
2016-08-26 21:07:27 -0700

    mkvmuxer_sample: Add support for Projection element.
    Part of the Spherical Video V2 draft specification:
    https://github.com/google/spatial-media/blob/master/docs/spherical-video-v2-rfc.md
    
    Change-Id: If8cc7a102933ca7fe81990919dbabe7db97812f8

--- mkvmuxer/mkvmuxer.cc
+++ mkvmuxer/mkvmuxer.cc
@@ -1528,7 +1528,6 @@ uint64_t VideoTrack::VideoPayloadSize() const {
                             static_cast<float>(frame_rate_));
   if (colour_)
     size += colour_->ColourSize();
-
   if (projection_)
     size += projection_->ProjectionSize();
 
